5f32dc3c3e11f68f5c8fe7d4a3de54dd7f0bc4e0
kirilllykov <lykov.kirill@gmail.com>
2014-05-15 10:31:09 +0200

    added mtl file usage to Atom2objTranslator, added scaling factor

--- scripts/restart2obj.py
+++ scripts/restart2obj.py
@@ -6,7 +6,7 @@ Created on Sep 18, 2013
 @author: kirill lykov
 '''
 import os
-from src.Atom2objTranslator import Atom2objTranslator
+from Atom2objTranslator import Atom2objTranslator
 
 # restart file name is like <core name>.restart.<iteration number>
 # the method returns  <core name>.<iteration number>
@@ -20,15 +20,42 @@ def getAtomName(fileName):
     res = getCoreName(fileName) + ".atom"
     return res
 
-def getObjName(fileName):
-    res = getCoreName(fileName) + ".obj"
-    return res 
+def getObjName(fileName, index):
+    ind = fileName.find('.')
+    assert(ind != -1)
+    res = fileName[:ind + 1]
+    res.replace('_', '-')
+    return res + str(index) + ".obj"
 
-files = [f for f in os.listdir('.') if (os.path.isfile(f) and f.find("restart") != -1)]
-translator = Atom2objTranslator()
+def getIteration(fileName):
+    itNumInd = fileName.rfind('.')
+    assert(itNumInd != -1)
+    return fileName[itNumInd+1:]
+
+print("restart2obj started")
+files = [f for f in os.listdir('.') if (os.path.isfile(f) and f.find(".restart") != -1)]
+
+filesSorted = list(tuple())
 for f in files:
-    atomFileName = getAtomName(f)
-    cmd = "restart2data " + f + " " + atomFileName
+    it = int(getIteration(f))
+    filesSorted.append((it, f))
+
+filesSorted.sort(key=lambda x: x[0])
+print("there are " + str(len(files)) + " restart files found")
+
+# colors specified in mlt file, assumed that mlt file is called the <coreName>.mlt
+# and one for all obj files
+colors = ("red", "blue", "blue")
+
+translator = Atom2objTranslator(0.1, 2, 2, colors)
+for f,ind in zip(filesSorted, range(0,len(filesSorted))):
+    atomFileName = getAtomName(f[1])
+    cmd = "restart2data " + f[1] + " " + atomFileName
+    
+    print(atomFileName + ": restart2atom")
     os.system(cmd)
-    translator.run(atomFileName, getObjName(f))
+    
+    print(atomFileName + ": atom2obj")
+    translator.run(atomFileName, getObjName(f[1], ind))
+    
     os.remove(atomFileName)
