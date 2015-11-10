9c1834ae21d904d27751fc6d7751cb66f5469b32
kirilllykov <lykov.kirill@gmail.com>
2015-11-10 17:27:28 +0100

    Modified gather vectors so it can work with arbitrary data type

--- utils/gather_containers.h
+++ utils/gather_containers.h
@@ -18,12 +18,11 @@ typedef std::map<int, int> MapIntInt;
 typedef typename MapIntInt::const_iterator CIterMapInt;
 typedef typename MapIntInt::iterator IterMapInt;
 
-typedef std::vector<int> VectorInt;
-typedef typename std::vector<int>::const_iterator CIterVectorInt;
-
-// TODO can be easily generalized for the case Container<T> if needed
-template<class Container>
-void allGatherUnionOfContainers(const Container& localVector, MPI_Comm communicator, Container& globalUnion);
+/**
+ * All gather sets and maps
+ */
+void allGatherUnionOfContainers(const SetInt& localSet, MPI_Comm communicator, SetInt& globalUnionSet);
+void allGatherUnionOfContainers(const MapIntInt& localMap, MPI_Comm communicator, MapIntInt& globalUnionVector);
 
 /**
  * Returns union of vectors from all cores in communicator, do all necessary memory allocation
@@ -38,16 +37,74 @@ void allGatherUnionOfContainers(const Container& localVector, MPI_Comm communica
     std::cout << std::endl;
     return;
  */
-template<>
-void allGatherUnionOfContainers(const VectorInt& localVector, MPI_Comm communicator, VectorInt& globalUnionVector);
-
-/**
- * The same but for sets
- */
-template<>
-void allGatherUnionOfContainers(const SetInt& localSet, MPI_Comm communicator, SetInt& globalUnionSet);
+template<class T>
+struct MPITrait;
 
 template<>
-void allGatherUnionOfContainers(const MapIntInt& localMap, MPI_Comm communicator, MapIntInt& globalUnionVector);
+struct MPITrait<int>
+{
+  MPI_Datatype dataType;
+  MPITrait() : dataType(MPI_INT) {}
+};
+
+template<>
+struct MPITrait<double>
+{
+  MPI_Datatype dataType;
+  MPITrait() : dataType(MPI_DOUBLE) {}
+};
+
+template<>
+struct MPITrait<float>
+{
+  MPI_Datatype dataType;
+  MPITrait() : dataType(MPI_FLOAT) {}
+};
+
+template<class T>
+static void allGatherUnionOfContainers(const std::vector<T>& localVector, MPI_Comm communicator, std::vector<T>& globalUnionVector)
+{
+  MPITrait<T> trait;
+  int participants = 0;
+  MPI_Comm_size(communicator, &participants); // if it is called on the core which doesn't belong to this comm, application crashes
+
+  std::vector<int> counts(participants, 0);
+  int localCount = localVector.size();
+  MPI_Allgather(&localCount, 1, MPI_INT, &counts[0], 1, MPI_INT, communicator);
+
+  int globalCount = 0;
+  std::vector<int> displs(participants, 0);
+  for (int i = 0; i < participants; ++i) {
+    displs[i] += globalCount;
+    globalCount += counts[i];
+  }
+
+  globalUnionVector.resize(globalCount, 0);
+  std::vector<T>& localWithoutConst = const_cast<std::vector<T>&>(localVector);
+  MPI_Allgatherv(&localWithoutConst[0], localCount, trait.dataType, &globalUnionVector[0], &counts[0], &displs[0], trait.dataType, communicator);
+}
+
+template<class T>
+static void gatherUnionOfContainers(const std::vector<T>& localVector, MPI_Comm communicator, int root, std::vector<T>& globalUnionVector)
+{
+  MPITrait<T> trait;
+  int participants = 0;
+  MPI_Comm_size(communicator, &participants); // if it is called on the core which doesn't belong to this comm, application crashes
+
+  std::vector<int> counts(participants, 0);
+  int localCount = localVector.size();
+  MPI_Gather(&localCount, 1, MPI_INT, &counts[0], 1, MPI_INT, root, communicator);
+
+  int globalCount = 0;
+  std::vector<int> displs(participants, 0);
+  for (int i = 0; i < participants; ++i) {
+    displs[i] += globalCount;
+    globalCount += counts[i];
+  }
+
+  globalUnionVector.resize(globalCount, 0);
+  std::vector<T>& localWithoutConst = const_cast<std::vector<T>&>(localVector);
+  MPI_Gatherv(&localWithoutConst[0], localCount, trait.dataType, &globalUnionVector[0], &counts[0], &displs[0], trait.dataType, root, communicator);
+}
 
 #endif /* GATHER_CONTAINERS_H_ */
